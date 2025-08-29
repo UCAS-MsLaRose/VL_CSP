8cdb2114113db3fc579b85c47cd0a0ef5023f01e
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-08-29 13:26:16 -0600

    Variables notes

--- notes 6th/variables.py
+++ notes 6th/variables.py
@@ -7,6 +7,7 @@ print("This is an output!")
 subject = "CSP"
 pi = 3.14
 color = "purple"
+print(pi)
 
 #snake_case (all lower case, spaces replaced with underscores)
 baseball_team = "Red Sox"
@@ -15,3 +16,20 @@ baseballTeam = "Pirates"
 #PascalCase (Capitilize the 1st letter of every word, squish words together)
 BaseballTeam = "Giants"
 
+#reassign a variable
+pi = 3.14159265
+
+pi = "French Silk"
+
+pi = "Pumpkin" 
+
+print(pi)
+
+name = input("What is your name?\n")
+
+quest = input("What is your quest: ")
+
+print("What is the average wing velocity of an unlaiden swollow?")
+velocity = input()
+
+print("Your name is", name, "This is", subject, "The best pie is", pi, "we all cheer for the", baseball_team, "the best color is", color, "Your quest is currently", quest, "and you must know the velocity of an unlaiden swallow is", velocity, 56415612561561)
