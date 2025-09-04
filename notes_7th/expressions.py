d017b712c2eb0e6ac05ddebd506b36b4ef0bca69
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-04 14:41:45 -0600

    expressions notes

---
+++ notes_7th/expressions.py
@@ -0,0 +1,37 @@
+# VL 7th Expressions notes
+
+# What is an algorithm?
+    # answer
+name = input("What is your name:\n")
+print("Hello", name)
+
+# Treyson has 12 apples, he has 5 friends that he wants to give apples to. How many apples does each friend get? 
+apples = 12
+friends = 5
+print("Each friend gets", apples/friends, "apples!")
+
+# List steps in an algorithm
+
+# Average age of a group of 4 people
+age_one = 55
+age_two = 89
+age_three = 99999999999999
+age_four = 4
+
+total_ages = age_one + age_two + age_three + age_four
+average = total_ages/4
+
+print("The average age is:", average)
+
+
+# List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+num_one = 12
+num_two = 7
+
+print("Addition(+): ", num_one+num_two)
+print("Subtraction(-): ", num_one-num_two)
+print("Multiplication(*): ", num_one*num_two)
+print("Division(/): ", num_one/num_two)
+print("Exponents(**): ", num_one**num_two)
+print("Integer Division(//): ", num_one//num_two)
+print("Modulo(%): ", num_one%num_two)
