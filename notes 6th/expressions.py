c0142b86b1b132b5c46dde5963e7f3dee6d4611f
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-04 13:43:43 -0600

    Started expressions notes

---
+++ notes 6th/expressions.py
@@ -0,0 +1,35 @@
+# VL 6th Expressions Notes! 
+
+# What is an algorithm?
+    # answer here
+name = input("What is your name? ")
+print("Hello", name)
+
+#algorithm for area
+length = 5
+width = 20
+area = length * width 
+print(length, "X", width, "=", area)
+
+#1
+age_one = 55
+age_two = 12
+age_three = 6
+age_four = 19
+#2
+total = age_one + age_two + age_three + age_four
+average = total/4
+#3
+print("The average age between:", age_one, "-", age_two, "-", age_three, "-", age_four, "=", average)
+
+#Math equations!
+num_one = 10
+num_two = 6
+
+print("addition:", num_one + num_two)
+print("subtraction:", num_one - num_two)
+print("multiplication:", num_one * num_two)
+print("division:", num_one / num_two)
+print("exponents:", num_one ** num_two)
+print("integer division:", num_one // num_two)
+print("modulo (remainder):", num_one % num_two)
