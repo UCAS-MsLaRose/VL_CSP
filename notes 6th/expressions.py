1b7a92321fb1d354301d8edc8a67297e6962df04
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-05 13:29:08 -0600

    Expressions notes

--- notes 6th/expressions.py
+++ notes 6th/expressions.py
@@ -23,13 +23,21 @@ average = total/4
 print("The average age between:", age_one, "-", age_two, "-", age_three, "-", age_four, "=", average)
 
 #Math equations!
-num_one = 10
-num_two = 6
+num_one = float(input("Give me a number:\n"))
+num_two = int(input("Give me a number:\n"))
 
-print("addition:", num_one + num_two)
-print("subtraction:", num_one - num_two)
-print("multiplication:", num_one * num_two)
-print("division:", num_one / num_two)
-print("exponents:", num_one ** num_two)
-print("integer division:", num_one // num_two)
-print("modulo (remainder):", num_one % num_two)
+num_one += num_two
+print("addition (+):", round(num_one, 0)) #1. What needs rounded 2. How many decimals
+num_one -= num_two
+print("subtraction(-):", num_one)
+num_one *= num_two
+print("multiplication(*):", num_one)
+num_one /= num_two
+print("division(/):", round(11/3, 2))
+num_one **= num_two
+print("exponents(**):", num_one)
+num_one //= num_two
+print("integer division(//):", num_one)
+num_one %= num_two
+print("modulo (remainder)(%):", num_one)
+print("(3*5**2/15)-(5-2**2)=",(3*5**2/15)-(5-2**2))
---
+++ practices/expressions_practice.py
@@ -0,0 +1,3 @@
+# VL 6th Expressions Practice
+
+print("", )
