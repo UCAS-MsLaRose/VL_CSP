ea08636212b5ebc75c73b7619c46003ec7dbd614
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-08 14:43:19 -0600

    strings notes

---
+++ notes_7th/strings.py
@@ -0,0 +1,14 @@
+# VL 7th String Notes
+
+#Examples
+first_name = input("What is your first name:\n").strip().title()
+
+last_name = input("What is your last name:\n").strip().title()
+
+full_name = first_name + " " + last_name
+
+sentence = '        The quick brown fox jumps over the lazy dog.       '
+print(sentence.strip())
+
+print("Welcome to my program", full_name + "!")
+
