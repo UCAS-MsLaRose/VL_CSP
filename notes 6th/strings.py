c1dce70ae4e0f0ddfe9cf2ac14ff48aa2921f3f2
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-09 13:44:33 -0600

    Finish strings notes

--- notes 6th/strings.py
+++ notes 6th/strings.py
@@ -10,8 +10,17 @@ full_name = first_name + " " + last_name
 sentence = "The quick brown fox jumps over the lazy dog."
 
 print("Welcome to my program", full_name + "!")
+print(sentence.find("brown"))
+print(sentence[10:15])
 
 # Debugging is fixing problems in my code!
     # Syntax Error
+string = 'This is my string'
     # Logic Error
+numOne = "1"
+numTwo = "3"
+print(numOne + numTwo)
     # Run-Time Error 
+
+letter = "a"
+#int(letter)
