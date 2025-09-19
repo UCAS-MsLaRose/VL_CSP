9c24b70a58b25db1bb2f98ed4f62fd506e6f4b12
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-19 13:12:40 -0600

    Imported datetime

---
+++ notes 6th/for_loops.py
@@ -0,0 +1,10 @@
+# VL 6th Time and For Loops notes!
+
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour
+
+#print(f"The time in seconds since Jan 1, 1970: {epoch}")
+print(f"The time is: {current}")
+print(f"The hour is: {hour}")
