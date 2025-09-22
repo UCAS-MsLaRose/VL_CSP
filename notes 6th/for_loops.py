f71600e8908667544828653690c0440bb7cfd863
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-22 14:45:05 -0600

    Loop notes

--- notes 6th/for_loops.py
+++ notes 6th/for_loops.py
@@ -10,4 +10,52 @@ print(f"The time is: {current}")
 print(f"The hour is: {hour}")
 
 # Lists
-siblings = ["Alex", "Katie", "Andrew", "Vienna", "Tia", "Treyson", "Xavier", "Jake", ["Joseph", "Noah", "Zee"], 8, 3.4158]
+siblings = ["Alex", "Katie", "Andrew", "Vienna", "Victoria", "Treyson", "Jefferson", "Jake"]
+
+print(siblings[3])
+print(siblings)
+siblings[4] = "Tia"
+siblings[6] = "Xavier"
+siblings.remove("Vienna")
+
+#For loop
+for sibling in siblings:
+    #print(f"Hello {sibling}")
+    print("Hi")
+
+for x in range(20,-11, -1):
+    print(x)
+
+#While Loops
+#infinite Loops
+#while True:
+#    print("Oh NO!")
+
+
+#Good While loop
+i = 1
+
+while i < 21:
+    if i % 2 == 0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
+    i += 1
+
+import random
+
+number = random.randint(1,20)
+x = 1
+"""while x != number:
+    print("Duck") 
+    x += 1
+
+print("Goose!")"""
+
+while True:
+    if number == x:
+        print("Goose!")
+        break
+    else:
+        print("Duck")
+        x += 1
