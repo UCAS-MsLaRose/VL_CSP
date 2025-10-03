c461739b071d27a8ba9adb9e2d99bce79109fd9e
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-10-03 13:44:28 -0600

    Variables notes

--- c_programming/notes/variables.c
+++ c_programming/notes/variables.c
@@ -1,14 +1,26 @@
 // VL 6th Varibles Notes
+/*
+this is a comment
+*/
 #include <stdio.h>
 
 int main(void){
-    int num = 4;
-    float pi = 3.14;
-    char grade = 'A'; //Will only hold 1 letter
-    char name[]= "LaRose";
+    int num;
+    const float pi = 3.14; //constants cannot be changed
+    char grade; //Will only hold 1 letter
+    char name[20];
     //bool passing = true;
+    printf("What is your letter grade: ");
+    scanf("%c", &grade);
 
-    printf("%d", num);
+    printf("Tell me a number: ");
+    scanf("%d", &num);
+    //This imput lets me get a space
+    printf("Tell me your name: ");
+    fgets(name, sizeof(name), stdin);
+
+    printf("%d\n", num);
+    printf("%s has a %c grade in the class!\n", name, grade);
 
     return 0;
 }
