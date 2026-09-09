2b08f5f8bba8d58d9e64aa94b312ef3a2b5c26ab
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-09-09 13:54:02 -0600

    variable notes

---
+++ 6th/variables_notes.py
@@ -0,0 +1,7 @@
+# VL, Variables Notes
+
+name = input("Tell me your name: ")
+
+print("CSP is the best class!")
+print("It is taught by the coolest teacher!")
+print("Hello", name, "welcome to class!")
