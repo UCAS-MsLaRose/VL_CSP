1a079e1ffad7c295ec3d9d3dc1b1281e28e8fb6e
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-09-11 13:53:01 -0600

    memory notes

---
+++ 6th/physical_logical_memory.py
@@ -0,0 +1,8 @@
+# VL, Physical and Logical Memory
+
+pi = 3.1415
+
+everything = 42
+
+print(f"The variable pi is {pi} and it is saved at {id(pi)}")
+print(f"The variable everything is {everything} and it is saved at {id(everything)}")
