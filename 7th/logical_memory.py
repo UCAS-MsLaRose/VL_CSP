c80aef2a50b30126b9624384a222f2c71e66311c
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-09-15 13:29:43 -0600

    fixing inputs

---
+++ 7th/logical_memory.py
@@ -0,0 +1,9 @@
+# VL, Logical and physical memory notes
+
+pi = 3.141592
+
+school = "UCAS"
+
+print(f"\nThe variable pi has a value of {pi} and is saved at {id(pi)}\n")
+
+print(f"\nThe variable school has a value of {school} and is saved at {id(school)}\n")
