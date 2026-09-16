de3eefd2ef47018982abd4016d28efc1a9d08a97
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-09-16 14:54:42 -0600

    expressions notes

---
+++ 6th/expressions.py
@@ -0,0 +1,28 @@
+# VL 6th Integers, Floats and Expressions Notes
+
+# Integers
+people = 18
+
+shoes = people * 2
+
+hand = people * 2 
+
+# Float 
+pi = 3.1415
+
+water = 60.5
+
+temp = 69.8
+
+price = 1.99
+
+# expression 
+print(water*price)
+print(f"Your {water} gallons of water costs ${round(water*price,2)}")
+
+#modulo and integer division
+print(f"18/4={18/4} which is {18//4} with a remainder of {18%4}")
+print(f"18/5={18/5} which is {18//5} with a remainder of {18%5}")
+print(f"18/6={18/6} which is {18//6} with a remainder of {18%6}")
+
+print(int(people*2*(5-1)/3))
