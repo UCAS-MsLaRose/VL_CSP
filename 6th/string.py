5f45f95c2a163b46fed77032f812d39c736d8638
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-09-18 14:55:29 -0600

    string notes

---
+++ 6th/string.py
@@ -0,0 +1,16 @@
+# VL String Notes
+last_name = 'LaRose'
+first_name = "Vienna"
+# concatenation => add two string together
+name = first_name + " " + last_name
+# escape char lets the program ignore the next character in the string 
+print(f'{name} told the class "You can\'t drive my car."')
+
+user = input("Please tell me your name:\n").strip().title()
+
+print(f"New user recognized\nWelcome {user}")
+
+sentence = "The quick brown fox jumped over the lazy dog."
+print(f"The sentence is {len(sentence)} characters long.")
+print(sentence)
+print(sentence.replace("dog", name))
---
+++ 7th/strings.py
@@ -0,0 +1,22 @@
+# VL Strings Notes 
+
+# string => any saved inside of quotation marks "" ''
+
+name = input("What is your name: ").strip().capitalize()
+
+age = input('How old are you: ')
+print(type(age))
+
+# Concatenation => puts two strings directly next to each other
+print(age+age)
+
+print(name + " " + "LaRose")
+
+# 
+sentence = "The quick brown fox jumped over the lazy dog."
+
+print(sentence)
+print(sentence.replace("dog", "monkey"))
+print(len(name)) #<= gets the length of a string
+
+print(f"Your name is {name} that is {len(name)} letters long. Your first initial is {name[0]} I think I will call you {name[0:3]}")
