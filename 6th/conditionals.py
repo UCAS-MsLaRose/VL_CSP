45d5883132486326284b02f17a63ab1804a48d3c
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-09-22 14:53:39 -0600

    conditionals notes

---
+++ 6th/conditionals.py
@@ -0,0 +1,29 @@
+# VL Conditional Notes
+
+military_time = 900
+
+if military_time < 600:
+    print("Its too early why are you awake!!!!")
+elif military_time < 900:
+    print("Good Morning!")
+elif military_time < 1200:
+    print("Good Morning! You should be at school!")
+elif military_time < 1700:
+    print("Good Afternoon")
+else:
+    print("Good Evening")
+
+#nesting conditionals
+day = "Saturday"
+time = 900
+
+if time > 900 and time < 1600:
+    if day != "Saturday" or day != "Sunday":
+        print("You should be at school!")
+    else:
+        if time > 1200:
+            print("Good Afternoon!")
+        else:
+            print("Good Morning!")
+else:
+    print("You are not required to be at school")
